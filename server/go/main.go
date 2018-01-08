62d6864745b41e913d6860b6fef37f48b20517e4
Vlad Dziuba <dzyubavlad@gmail.com>
2018-01-08 11:03:47 +0200

    Implement time measuring

--- server/go/main.go
+++ server/go/main.go
@@ -6,20 +6,26 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"path"
+	"time"
 )
 
 func simulation(resp http.ResponseWriter, req *http.Request) {
 	log.Println("request start")
-	cmd := exec.Command("java", "-classpath", "ParallelDESS.jar", "cli.NetSimulator")
+	startTime := time.Now()
+	jarPath := path.Join("server", "go", "ParallelDESS.jar")
+	cmd := exec.Command("java", "-classpath", jarPath, "cli.NetSimulator")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		log.Printf("ERROR: cmd.StdinPipe(): %v", err)
+		resp.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		log.Printf("ERROR: cmd.StderrPipe(): %v", err)
+		resp.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	var errOutput []byte
@@ -37,20 +43,24 @@ func simulation(resp http.ResponseWriter, req *http.Request) {
 	_, err = io.Copy(stdin, req.Body)
 	if err != nil {
 		log.Printf("ERROR: io.Copy(): %v", err)
+		resp.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	log.Println("Run")
 	output, err := cmd.Output()
 	if err != nil {
 		log.Printf("ERROR: cmd.Output(): %v: %q, stderr: %q", err, string(output), string(errOutput))
+		resp.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	log.Println("duration: ", time.Now().Sub(startTime))
 	log.Println("Copy from stdout")
 
 	_, err = resp.Write(output)
 	if err != nil {
 		log.Printf("ERROR: resp.Write(): %v", err)
+		resp.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -58,7 +68,7 @@ func simulation(resp http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("../../public_html")))
+	http.Handle("/", http.FileServer(http.Dir("public_html")))
 	http.HandleFunc("/simulator", simulation)
 
 	log.Println("Server has been started on http://localhost:8080/")
